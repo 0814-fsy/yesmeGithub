c19da512a3c99e22c4ada446b3949b1d8c46974d
fsy <1465844837@qq.com>
2022-12-21 14:54:41 +0800

    1.0

---
+++ assets/online.5aeb1ab8.js
@@ -0,0 +1,1 @@
+import{H as t}from"./index.41c72bba.js";function r(o){return t({url:"/monitor/online/list",method:"get",params:o})}function n(o){return t({url:"/monitor/online/"+o,method:"delete"})}export{n as f,r as l};
